f2a884b4bb85ac152168945d72e7692c244aa73f
Sean Gray <seangray.dev@gmail.com>
2023-06-28 01:15:25 -0400

    modify button styles + add dark mode

--- src/components/Layout/SignIn.tsx
+++ src/components/Layout/SignIn.tsx
@@ -5,9 +5,13 @@ const SignInPage = () => {
   return (
     <main className='grid place-items-center h-screen w-full'>
       <div className='flex flex-col gap-10'>
-        <img src={'/images/logo-no-background-light.png'} width={500} />
         <img
-          className='hidden'
+          className='dark:hidden'
+          src={'/images/logo-no-background-light.png'}
+          width={500}
+        />
+        <img
+          className='dark:block hidden'
           src={'/images/logo-no-background.png'}
           width={500}
         />
