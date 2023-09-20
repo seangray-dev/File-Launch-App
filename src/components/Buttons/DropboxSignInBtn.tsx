c7394add9dad63617d037f0ce47413030617c559
Sean Gray <seangray.dev@gmail.com>
2023-09-19 20:04:31 -0400

    fix text color on signin comp

--- src/components/Buttons/DropboxSignInBtn.tsx
+++ src/components/Buttons/DropboxSignInBtn.tsx
@@ -1,12 +1,12 @@
 const DropboxSignInBtn = () => {
-  return (
-    <button
-      className='w-2/3 mx-auto bg-white rounded-md text-deepBlue font-bold flex gap-4 py-3 justify-center items-center border border-deepBlue hover:border-gray hover:text-gray transition-all duration-300'
-      onClick={() => {}}>
-      <img src='./images/dropbox-logo.png' alt='' />
-      <span>Sign In with Dropbox</span>
-    </button>
-  );
+	return (
+		<button
+			className='w-2/3 mx-auto bg-white rounded-md text-black font-bold flex gap-4 py-3 justify-center items-center border border-deepBlue hover:border-gray hover:text-gray transition-all duration-300'
+			onClick={() => {}}>
+			<img src='./images/dropbox-logo.png' alt='' />
+			<span>Sign In with Dropbox</span>
+		</button>
+	);
 };
 
 export default DropboxSignInBtn;
--- src/components/Buttons/GoogleSignInBtn.tsx
+++ src/components/Buttons/GoogleSignInBtn.tsx
@@ -1,20 +1,20 @@
 import { login } from '../../services/auth';
 
 const GoogleSignInBtn = () => {
-  const handleClick = () => {
-    login().catch((error) => {
-      console.error('Login failed:', error);
-    });
-  };
+	const handleClick = () => {
+		login().catch((error) => {
+			console.error('Login failed:', error);
+		});
+	};
 
-  return (
-    <button
-      onClick={handleClick}
-      className='w-2/3 mx-auto bg-white rounded-md text-deepBlue font-bold flex gap-4 py-3 justify-center items-center border border-deepBlue hover:border-gray hover:text-gray transition-all duration-300'>
-      <img className='w-6' src='./images/google-logo.png' alt='' />
-      <span>Sign In with Google</span>
-    </button>
-  );
+	return (
+		<button
+			onClick={handleClick}
+			className='w-2/3 mx-auto bg-white rounded-md text-black font-bold flex gap-4 py-3 justify-center items-center border border-deepBlue hover:border-gray hover:text-gray transition-all duration-300'>
+			<img className='w-6' src='./images/google-logo.png' alt='' />
+			<span>Sign In with Google</span>
+		</button>
+	);
 };
 
 export default GoogleSignInBtn;
