97f4d4d2a6b98f270647c5cb1e5d2a1637a2fa3d
Sean Gray <seangray.dev@gmail.com>
2023-09-27 21:29:58 -0400

    use redux state to track playback in slider component, add currentTime and total duration

--- src/components/Layout/AudioPlayer/ProgressSlider.tsx
+++ src/components/Layout/AudioPlayer/ProgressSlider.tsx
@@ -1,18 +1,61 @@
 import { Slider } from '@/components/ui/slider';
+import {
+	audioBuffer,
+	offsetTime,
+	startTime,
+	updatePlaybackPosition,
+} from '@/redux/features/currentFile-slice';
 import { RootState } from '@/redux/store';
-import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { audioContext } from '@/utils/audioUtils';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+const formatTime = (time: number) => {
+	const minutes = Math.floor(time / 60);
+	const seconds = Math.floor(time % 60);
+	return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+};
 
 const ProgressSlider = () => {
-	const isPlaying = useSelector((state) => state.currentFile.isPlaying);
-	const duration = useSelector((state) => state.currentFile.duration);
-	const [totalDuration, setTotalDuration] = useState(100); // in seconds
+	const dispatch = useDispatch();
+	const isPlaying = useSelector(
+		(state: RootState) => state.currentFile.isPlaying
+	);
+	const [totalDuration, setTotalDuration] = useState(0); // in seconds
 	const [currentTime, setCurrentTime] = useState(0); // in seconds
 
+	useEffect(() => {
+		if (audioBuffer) {
+			setTotalDuration(audioBuffer.duration);
+		}
+
+		const intervalId = setInterval(() => {
+			// Add a check to see if the audio has finished
+			const currentTimeCalculation =
+				audioContext.currentTime - startTime + offsetTime;
+
+			if (
+				isPlaying &&
+				audioContext &&
+				typeof startTime !== 'undefined' &&
+				currentTimeCalculation <= totalDuration
+			) {
+				setCurrentTime(currentTimeCalculation);
+			} else {
+				// Stop the interval when audio finishes
+				clearInterval(intervalId);
+			}
+			// Update every second
+		}, 1000);
+
+		// Clear the interval when the component unmounts
+		return () => clearInterval(intervalId);
+	}, [isPlaying, totalDuration]);
+
 	const handlePlaybackChange = (value: number[]) => {
-		// Update the audio playback position here
-		// Assuming value is an array with one element
-		setCurrentTime(value[0]);
+		const newTime = value[0];
+		setCurrentTime(newTime);
+		dispatch(updatePlaybackPosition(newTime) as any);
 	};
 
 	return (
@@ -25,10 +68,14 @@ const ProgressSlider = () => {
 				step={1}
 				onValueChange={handlePlaybackChange}
 			/>
-			{/* <div>
-				<p>Is Playing: {isPlaying ? 'Yes' : 'No'}</p>
-				<p>Duration: {duration !== null ? `${duration} seconds` : 'Unknown'}</p>
-			</div> */}
+			<div className='w-full pt-2 px-2 flex justify-between'>
+				<div className='text-xs text-muted-foreground'>
+					{formatTime(currentTime)}
+				</div>
+				<div className='text-xs text-muted-foreground'>
+					{formatTime(totalDuration)}
+				</div>
+			</div>
 		</div>
 	);
 };
