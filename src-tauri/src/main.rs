4ba5487cbf1b52bb3c4ad2d94f07659821d36fdb
Sean Gray <seangray.dev@gmail.com>
2023-10-11 15:24:31 -0400

    refactor scan_directory_recursive + add logs for time to execute scan_directory func

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,6 +13,7 @@ use std::io::BufReader;
 use rodio::source::Source;
 use once_cell::sync::Lazy;
 use std::sync:: Mutex;
+use std::time::Instant;
 
 
 
@@ -72,91 +73,94 @@ async fn select_directory() -> Result<String, String> {
 
 #[tauri::command]
 async fn scan_directory(base_folder: String) -> Result<Vec<HashMap<String, String>>, String> {
+    let start = Instant::now();
     // Check if preloaded files exist
     if let Some(preloaded_files) = PRELOADED_FILES.lock().unwrap().clone() {
         return Ok(preloaded_files);
     }
 
     let mut files: Vec<HashMap<String, String>> = vec![];
-    // Supplying None for days_filter so it uses the default of 7 days.
+    // Supplying None for days_filter so it uses the default of 30 days.
     if let Err(e) = scan_directory_recursive(Path::new(&base_folder), &mut files, None) {
         return Err(format!("Failed to read directory: {}", e));
     }
+    let duration = start.elapsed();
+    println!("Time elapsed for Option 1: {:?}", duration);
     Ok(files)
 }
 
 fn scan_directory_recursive(dir: &Path, files: &mut Vec<HashMap<String, String>>, days_filter: Option<u64>) -> std::io::Result<()> {
+    if !dir.is_dir() {
+        return Ok(());
+    }
+
     let current_time = SystemTime::now();
-    let filter_duration = match days_filter {
-        Some(days) => Duration::from_secs(days * 24 * 60 * 60),
-        None => Duration::from_secs(30 * 24 * 60 * 60),  // Default to 30 days
-    };
-    
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            
-            println!("Processing path: {:?}", path);
+    let filter_duration = days_filter.map_or(Duration::from_secs(30 * 24 * 60 * 60), |days| {
+        Duration::from_secs(days * 24 * 60 * 60)
+    });
 
-            if path.is_dir() {
-                scan_directory_recursive(&path, files, days_filter)?;
-            } else {
-                let metadata: Metadata = fs::metadata(&path)?;
-                if let Some(ext) = path.extension() {
-                    if let Some(ext_str) = ext.to_str() {
-                        // Check for mp3 or wav
-                        if ext_str == "mp3" || ext_str == "wav" {
-                            // Check if the file is stereo
-                            let channels = check_audio_channels(path.to_str().unwrap().to_string());
-                            if channels == "stereo" {
-                                // Check for the last modified time within the filter range
-                                if let Ok(time) = metadata.modified() {
-                                    if let Ok(duration) = time.duration_since(UNIX_EPOCH) {
-                                        let modified_duration = current_time.duration_since(UNIX_EPOCH).unwrap() - duration;
-                                        if modified_duration > filter_duration {
-                                            println!("Skipping file (older than filter): {:?}", path);
-                                            continue;  // Skip this file if it's older than filter
-                                        }
-                                    }
-                                }
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        println!("Processing path: {:?}", path);
 
-                                let mut file_obj = HashMap::new();
-                                // Add name
-                                if let Some(name) = path.file_name() {
-                                    if let Some(name_str) = name.to_str() {
-                                        file_obj.insert("name".to_string(), name_str.to_string());
-                                    }
-                                }
+        if path.is_dir() {
+            scan_directory_recursive(&path, files, days_filter)?;
+            continue;
+        }
 
-                                // Add parent
-                                if let Some(parent) = path.parent() {
-                                    if let Some(parent_str) = parent.to_str() {
-                                        file_obj.insert("parent".to_string(), parent_str.to_string());
-                                    }
-                                }
+        let metadata: Metadata = fs::metadata(&path)?;
+        if metadata.modified().map_or(true, |time| {
+            time.duration_since(UNIX_EPOCH).map_or(true, |duration| {
+                current_time.duration_since(UNIX_EPOCH).unwrap() - duration > filter_duration
+            })
+        }) {
+            println!("Skipping file (older than filter): {:?}", path);
+            continue;
+        }
 
-                                // Add path
-                                if let Some(path_str) = path.to_str() {
-                                    file_obj.insert("path".to_string(), path_str.to_string());
-                                }
-
-                                file_obj.insert("lastModified".to_string(), metadata.modified().unwrap().duration_since(UNIX_EPOCH).unwrap().as_secs().to_string());
-                                file_obj.insert("fileType".to_string(), ext_str.to_string());
-
-                                println!("Adding stereo file to list: {:?}", path);
-                                files.push(file_obj);
-                            }
+        if let Some(ext_str) = path.extension().and_then(|ext| ext.to_str()) {
+            match ext_str {
+                "mp3" | "wav" => {
+                    // Check if the file's parent directory is "Bounced Files"
+                    // Add additional default export folders for DAWS
+                    if let Some(parent) = path.parent() {
+                        if parent.file_name().and_then(|name| name.to_str()) != Some("Bounced Files") {
+                            continue;
                         }
                     }
+                    // Check if the file is stereo
+                    if check_audio_channels(path.to_str().unwrap().to_string()) != "stereo" {
+                        continue;
+                    }
+
+                    let mut file_obj = HashMap::new();
+
+                    // Add name
+                    if let Some(name_str) = path.file_name().and_then(|name| name.to_str()) {
+                        file_obj.insert("name".to_string(), name_str.to_string());
+                    }
+                    // Add parent path
+                    if let Some(parent_str) = path.parent().and_then(|parent| parent.to_str()) {
+                        file_obj.insert("parent".to_string(), parent_str.to_string());
+                    }
+                    // Add path
+                    if let Some(path_str) = path.to_str() {
+                        file_obj.insert("path".to_string(), path_str.to_string());
+                    }
+
+                    file_obj.insert("lastModified".to_string(), metadata.modified().unwrap().duration_since(UNIX_EPOCH).unwrap().as_secs().to_string());
+                    file_obj.insert("fileType".to_string(), ext_str.to_string());
+                    println!("Adding stereo file to list: {:?}", path);
+                    files.push(file_obj);
                 }
+                _ => {}
             }
         }
     }
+
     Ok(())
 }
-
-
 
 #[tauri::command]
 fn check_audio_channels(path: String) -> String {
