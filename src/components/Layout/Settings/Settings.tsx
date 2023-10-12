e60ebf9abc5e14b84cf098c38b81a9247d9c39e7
Sean Gray <seangray.dev@gmail.com>
2023-10-12 19:11:35 -0400

    rename and forward Settings component using barrel pattern

---
+++ src/components/Layout/Settings/Settings.tsx
@@ -0,0 +1,20 @@
+import BaseFolder from './BaseFolder';
+import DefaultFilterDuration from './DefaultFilterDuration';
+import Startup from './Startup';
+import Theme from './Theme';
+
+const Settings = () => {
+	return (
+		<div className='flex flex-col gap-10'>
+			<h1 className='font-bold text-2xl'>Settings</h1>
+			<div className='flex flex-col gap-10'>
+				<Theme />
+				<BaseFolder />
+				<DefaultFilterDuration />
+				<Startup />
+			</div>
+		</div>
+	);
+};
+
+export default Settings;
