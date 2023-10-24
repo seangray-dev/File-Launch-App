d18fe3f26a5b6585291d701a10a1118c12a9784a
Sean Gray <seangray.dev@gmail.com>
2023-10-24 19:47:34 -0400

    apply spaces instead of indents

--- src/components/Pages/RecentFiles/RecentFiles.tsx
+++ src/components/Pages/RecentFiles/RecentFiles.tsx
@@ -7,35 +7,35 @@ import Files from './Files';
 import NoBaseFolder from './NoBaseFolder';
 
 const RecentFiles = () => {
-	// Redux State
-	const dispatch: AppDispatch = useDispatch();
-	const { baseFolder, files, areFilesChecked, status } = useSelector(
-		(state: RootState) => state.recentFiles
-	);
+  // Redux State
+  const dispatch: AppDispatch = useDispatch();
+  const { baseFolder, files, areFilesChecked, status } = useSelector(
+    (state: RootState) => state.recentFiles
+  );
 
-	useEffect(() => {
-		if (baseFolder) {
-			dispatch(fetchFiles(baseFolder));
-		}
-	}, [dispatch, baseFolder]);
+  useEffect(() => {
+    if (baseFolder) {
+      dispatch(fetchFiles(baseFolder));
+    }
+  }, [dispatch, baseFolder]);
 
-	return (
-		<div className='dark:text-white'>
-			{!baseFolder ? (
-				<NoBaseFolder />
-			) : (
-				<Files recentFiles={files} areFilesChecked={areFilesChecked} />
-			)}
+  return (
+    <div className='dark:text-white'>
+      {!baseFolder ? (
+        <NoBaseFolder />
+      ) : (
+        <Files recentFiles={files} areFilesChecked={areFilesChecked} />
+      )}
 
-			{!areFilesChecked && (
-				<div className='flex justify-center items-center h-32'>
-					{status === 'loading' ? (
-						<Loader2 className='h-10 w-10 animate-spin' />
-					) : null}
-				</div>
-			)}
-		</div>
-	);
+      {!areFilesChecked && (
+        <div className='flex justify-center items-center h-32'>
+          {status === 'loading' ? (
+            <Loader2 className='h-10 w-10 animate-spin' />
+          ) : null}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default RecentFiles;
