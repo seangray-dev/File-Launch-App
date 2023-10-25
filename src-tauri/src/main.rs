c0c26f2c36ad9769c6d0aa03489d8208608ceb71
Sean Gray <seangray.dev@gmail.com>
2023-10-25 16:53:37 -0400

    install fs-extra

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -30,6 +30,7 @@ fn main() {
         .plugin(tauri_plugin_persisted_scope::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_fs_watch::init())
+        .plugin(tauri_plugin_fs_extra::init())
         .setup(|app| {
             // Get the path to the app's configuration directory
             let config_dir = app.path_resolver().app_config_dir().ok_or_else(|| {
