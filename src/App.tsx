9d0488d98a5adb36dd3adb6eccf3228f93df4590
Sean Gray <seangray.dev@gmail.com>
2023-10-25 12:23:53 -0400

    pass setCurrentView as props + fix indentations

--- src/App.tsx
+++ src/App.tsx
@@ -15,61 +15,61 @@ import { ReduxProvider } from './redux/provider';
 import { logout } from './services/auth';
 
 function App() {
-	const [currentView, setCurrentView] = useState(() => {
-		const savedStartupView = window.localStorage.getItem('startupView');
-		return savedStartupView ? savedStartupView : 'default-view';
-	});
+  const [currentView, setCurrentView] = useState(() => {
+    const savedStartupView = window.localStorage.getItem('startupView');
+    return savedStartupView ? savedStartupView : 'default-view';
+  });
 
-	let componentInView;
-	switch (currentView) {
-		case 'Recent Files':
-			componentInView = <RecentFiles setCurrentView={setCurrentView} />;
-			break;
-		case 'Recipients':
-			componentInView = <Recipients />;
-			break;
-		case 'Format Files':
-			componentInView = <FormatFiles />;
-			break;
-		case 'Email Templates':
-			componentInView = <EmailTemplates />;
-			break;
-		case 'User Profile':
-			componentInView = <UserProfile />;
-			break;
-		case 'Settings':
-			componentInView = <Settings />;
-			break;
-		case 'Stats':
-			componentInView = <Stats />;
-			break;
-	}
+  let componentInView;
+  switch (currentView) {
+    case 'Recent Files':
+      componentInView = <RecentFiles setCurrentView={setCurrentView} />;
+      break;
+    case 'Recipients':
+      componentInView = <Recipients />;
+      break;
+    case 'Format Files':
+      componentInView = <FormatFiles />;
+      break;
+    case 'Email Templates':
+      componentInView = <EmailTemplates />;
+      break;
+    case 'User Profile':
+      componentInView = <UserProfile />;
+      break;
+    case 'Settings':
+      componentInView = <Settings />;
+      break;
+    case 'Stats':
+      componentInView = <Stats />;
+      break;
+  }
 
-	return (
-		<AuthProvider>
-			<ThemeProvider>
-				<ReduxProvider>
-					<main className='min-h-screen grid grid-cols-[200px_1fr] grid-rows-[auto_1fr_auto]'>
-						<div data-tauri-drag-region className='faux-header'></div>
-						<>
-							<section>
-								<SideBar setCurrentView={setCurrentView} />
-							</section>
-							<section className='flex flex-col h-full'>
-								<Header logout={logout} setCurrentView={setCurrentView} />
-								<div className='flex-grow grid overflow-y-auto'>
-									{componentInView}
-								</div>
-								<div className='bottom-0 sticky z-50'>
-									<AudioPlayer />
-								</div>
-							</section>
-						</>
-					</main>
-				</ReduxProvider>
-			</ThemeProvider>
-		</AuthProvider>
-	);
+  return (
+    <AuthProvider>
+      <ThemeProvider>
+        <ReduxProvider>
+          <main className='min-h-screen grid grid-cols-[200px_1fr] grid-rows-[auto_1fr_auto]'>
+            <div data-tauri-drag-region className='faux-header'></div>
+            <>
+              <section>
+                <SideBar setCurrentView={setCurrentView} />
+              </section>
+              <section className='flex flex-col h-full'>
+                <Header logout={logout} setCurrentView={setCurrentView} />
+                <div className='flex-grow grid overflow-y-auto'>
+                  {componentInView}
+                </div>
+                <div className='bottom-0 sticky z-50'>
+                  <AudioPlayer />
+                </div>
+              </section>
+            </>
+          </main>
+        </ReduxProvider>
+      </ThemeProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
