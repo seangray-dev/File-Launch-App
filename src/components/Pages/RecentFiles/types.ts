ad1266425e6accf8b5b5c53d5bfbdfbefa01a0f4
Sean Gray <seangray.dev@gmail.com>
2023-10-25 12:22:56 -0400

    add types.ts file for RecentFiles

---
+++ src/components/Pages/RecentFiles/types.ts
@@ -0,0 +1,7 @@
+export type RecentFilesProps = {
+  setCurrentView: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export type NoBaseFolderProps = {
+  setCurrentView: React.Dispatch<React.SetStateAction<string>>;
+};
