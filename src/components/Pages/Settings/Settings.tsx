05249592519ce66b93a250b0d76c48536a0f3237
Sean Gray <seangray.dev@gmail.com>
2023-11-01 04:18:09 -0400

    fix indentation

--- src/components/Pages/Settings/Settings.tsx
+++ src/components/Pages/Settings/Settings.tsx
@@ -4,17 +4,17 @@ import Startup from './Startup';
 import Theme from './Theme';
 
 const Settings = () => {
-	return (
-		<div className='flex flex-col gap-10 p-4'>
-			<h1 className='font-bold text-2xl'>Settings</h1>
-			<div className='flex flex-col gap-10'>
-				<Theme />
-				<BaseFolder />
-				<DefaultFilterDuration />
-				<Startup />
-			</div>
-		</div>
-	);
+  return (
+    <div className='flex flex-col gap-10 p-4'>
+      <h1 className='font-bold text-2xl'>Settings</h1>
+      <div className='flex flex-col gap-10'>
+        <Theme />
+        <BaseFolder />
+        <DefaultFilterDuration />
+        <Startup />
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
