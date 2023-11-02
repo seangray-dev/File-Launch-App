52fd73408149362d4dd7fe699fd4e815b3003941
Sean Gray <seangray.dev@gmail.com>
2023-11-02 00:08:20 -0400

    move Google + Dropbox sign in Buttons inside of supabase auth folder

---
+++ src/services/supabase/DropboxSignInBtn.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button';
+
+const DropboxSignInBtn = ({ onLogin }) => {
+  const handleClick = () => {
+    onLogin('google')();
+  };
+
+  return (
+    <Button
+      className='w-2/3 mx-auto bg-white rounded-md text-black flex gap-4 py-6 justify-center items-center border hover:bg-secondary hover:dark:text-white transition-all duration-300'
+      onClick={handleClick}>
+      <img src='./images/dropbox-logo.png' alt='' />
+      <span>Sign In with Dropbox</span>
+    </Button>
+  );
+};
+
+export default DropboxSignInBtn;
---
+++ src/services/supabase/GoogleSignInBtn.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/ui/button';
+
+const GoogleSignInBtn = ({ onLogin }) => {
+  const handleClick = () => {
+    onLogin('google')();
+  };
+
+  return (
+    <Button
+      onClick={handleClick}
+      className='w-2/3 mx-auto bg-white rounded-md text-black flex gap-4 py-6 justify-center items-center border hover:bg-secondary hover:dark:text-white transition-all duration-300'>
+      <img className='w-6' src='./images/google-logo.png' alt='' />
+      <span>Sign In with Google</span>
+    </Button>
+  );
+};
+
+export default GoogleSignInBtn;
+  
